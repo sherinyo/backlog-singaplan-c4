3a9e701d9b44fac7e1b4789a958d5494f7642d23
sherinyo <sherinyonatan@gmail.com>
2026-08-31 09:31:21 +0700

    Initial Commit

---
+++ backlog-singaplan/backlog_singaplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  backlog_singaplanApp.swift
+//  backlog-singaplan
+//
+//  Created by Sherin Alvinia Yonatan on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct backlog_singaplanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
